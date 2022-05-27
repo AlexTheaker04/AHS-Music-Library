8d6b3e842d8123b93c2888f8b896e9cc936d9612
Alex <74675759+AlexTheaker04@users.noreply.github.com>
2022-05-27 14:03:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import gspread
-import tkinter
+import tkinter as tk
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
+window = tk.Tk()
+text_box = tk.Text()
+text_box.pack()
+
+greeting = tk.Label(text="bogo is the best")
+greeting.pack()
+
+
+
 
 f = open("file.csv", "w")
 
@@ -17,8 +26,8 @@ records_data = sheet.get_all_records()
 df = pd.read_csv("test123 - Sheet1.csv")
 print(df.to_string())
 
-
 # for i in range(1, 263, 1):
 # print(list_of_hashes[i])
 
 # print(list_of_hashes[1])
+window.mainloop()
