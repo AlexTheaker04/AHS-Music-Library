e06eaffa55481122fb1e84dbb2c69ba29b3c5436
Alex <74675759+AlexTheaker04@users.noreply.github.com>
2022-05-27 13:49:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import gspread
+import tkinter
+import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
+
+f = open("file.csv", "w")
+
+
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
 client = gspread.authorize(creds)
 
 sheet = client.open('test123').sheet1
-list_of_hashes = sheet.get_all_records()
-for i in range(2, 265, 1):
-    print(list_of_hashes[i])
+records_data = sheet.get_all_records()
 
-print(list_of_hashes[1])
+df = pd.read_csv("test123 - Sheet1.csv")
+print(df.to_string())
+
+
+# for i in range(1, 263, 1):
+# print(list_of_hashes[i])
+
+# print(list_of_hashes[1])
