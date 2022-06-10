3aaf5b863e080deff592cf976c87baf0b8787e67
Alex <74675759+AlexTheaker04@users.noreply.github.com>
2022-06-10 14:27:00 -0400

    Update bogho.py
    nice colors

--- bogho.py
+++ bogho.py
@@ -4,19 +4,41 @@ import sys
 import csv  
 import webbrowser
 
-
 # declare class
-class box:
+class cupboard:
 
-    def __init__(self,box_num, X,Y):
+    def __init__(self,box_num, X, Y, W, H):
         self.box_num = box_num
         self.x = X
         self.y = Y
+        self.w = W
+        self.h = H
+        
+    def reveal(self):
+        pygame.draw.rect(screen, (255,0,0), (self.x, self.y, self.w, self.h))
+    def hide(self):
+        screen.blit(image, (400, 200))  
 
 # open and make list of csv file
 with open("AHS Jazz Music Library - Jazz - Instrument Storage.csv", "r") as f:
     csv_reader = csv.DictReader(f)
     name_records = list(csv_reader)
+
+
+
+def locate(index_num):
+    index_num = int(index_num)
+    if (0<= index_num <= 10):
+        return 1
+    elif (11 <=  index_num <=20):
+        return 2
+    elif (21 <= index_num <=30):
+        return 3
+    elif (31 <= index_num <=40):
+        return 4
+    elif (41 <= index_num <=40):
+        return 5
+    
 
 
 # when go to recording button is pressed, go to link or if no link to generic site
@@ -38,13 +60,13 @@ def show(test):
          string.append(f'{temp2[i]}: {temp[i]}')
     # print(string)
     for iter, thing in enumerate(string):
-        screen.blit(base_font.render(thing, True, (0,0,0)), (5,25*iter))
+        screen.blit(base_font.render(thing, True, (0,0,0)), (6,25*iter))
          
 # pygame.init() will initialize all
 # imported module
 pygame.init()
 clock = pygame.time.Clock()
-display_surface = pygame.display.set_mode((100, 100))
+# screen = pygame.display.set_mode((100, 100))
 screen = pygame.display.set_mode([1280, 720])
 
 # display icon and caption text
@@ -76,8 +98,14 @@ color_passive = pygame.Color('gainsboro')
 color = color_passive
   
 active = False
+box_1 = cupboard(1,410, 210, 190, 210)
+box_2 = cupboard(2,600,210,190,210)
+box_3 = cupboard(2,790,210,190,210)
+box_4 = cupboard(4,1024,210,190,210)
+# box_5 = cupboard(5,80,80)
+# box_6 = cupboard(6,100,100)
 
-
+if_draw = False
 while True:
     for event in pygame.event.get():
   
@@ -100,6 +128,7 @@ while True:
 
             if event.key == pygame.K_RETURN: # if key is the enter key
                 active = False
+                box_1.reveal()
             # Check for backspace
             if event.key == pygame.K_BACKSPACE:
   
@@ -111,6 +140,7 @@ while True:
                     temp = event.unicode
                     temp = int(temp)
                     user_text += event.unicode
+                    print(locate(user_text))
                 except:
                     print("letter")
     
@@ -125,7 +155,7 @@ while True:
          show(user_text)
         
     urlbutton = screen.blit(base_font.render("Go to recording", True, link_color), (5, 200))
-    display_surface.blit(text, textRect)
+    screen.blit(text, textRect)
     # draw rectangle and argument passed which should
     # be on screen
     pygame.draw.rect(screen, color, input_rect)
@@ -133,8 +163,27 @@ while True:
     text_surface = base_font.render(user_text, True, (255, 255, 255))
       
     screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
-    display_surface.blit(image, (400, 200))  
+    screen.blit(image, (400, 200))  
     # input_rect.w = max(100, text_surface.get_width()+10)
+
+
+    # display text
+    if len(user_text) >0:
+        
+        if locate(user_text) == 1:
+            box_1.reveal()
+            if_draw = True
+        elif locate(user_text) == 2:
+            box_2.reveal()
+            if_draw = True
+        elif locate(user_text) == 3:
+            box_3.reveal()
+        elif locate(user_text) == 4:
+            box_4.reveal()
+
+    
+    
+
       
     pygame.display.flip()
     clock.tick(60)
